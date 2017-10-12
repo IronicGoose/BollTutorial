956669eca13f06fb6c8c908790709771f0580ceb
IronicGoose <1282318378@qq.com>
2017-10-12 20:45:01 +0800

    SimplyMoveAdded
    See the scripts in script folder .Use input to controll the boll .

---
+++ Assets/Script/CatController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatController : MonoBehaviour {
+    public Rigidbody2D rigBody;
+    public bool force = false,isPressing;
+    public float minSpeed, maxSpeed, changeSpeed, decreseSpeed;
+    
+	// Use this for initialization
+	void Start () {
+        rigBody = GetComponent<Rigidbody2D>();
+	}
+	
+
+	// Update is called once per frame
+	void Update () {
+        if (force)
+        {
+            force = false;
+            rigBody.AddForce(new Vector2(0,5));
+        }
+        if(!isPressing)
+            rigBody.velocity = Vector2.Lerp(rigBody.velocity, rigBody.velocity.normalized * minSpeed, decreseSpeed * Time.deltaTime);
+        if (isPressing)
+            isPressing = false;
+	}
+    public void SetSpeed(Vector2 sp)
+    {
+        rigBody.velocity =  Vector2.Lerp(rigBody.velocity, 
+            rigBody.velocity .normalized * minSpeed+(maxSpeed - minSpeed) * sp ,changeSpeed * Time.deltaTime ) ;
+        isPressing = true;
+    }
+    
+
+}
---
+++ Assets/Script/InputHandler.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputHandler : MonoBehaviour {
+
+    public CatController cat;
+    public Vector2 speed;
+    private void Start()
+    {
+        if(cat == null)
+        {
+            Debug.Log("cat not set ");
+            enabled = false;
+        }
+    }
+    private void Update()
+    {
+        speed.x = Input.GetAxis("Horizontal");
+        speed.y = Input.GetAxis("Vertical");
+        if ( speed.sqrMagnitude >= 0.5 )
+        {
+            cat.SetSpeed(speed);
+        }
+    }
+}
